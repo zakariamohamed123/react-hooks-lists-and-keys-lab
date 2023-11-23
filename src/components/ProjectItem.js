6f624903b47e62c38cbf3946a550ae4d5bb2face
zakaria <zakimoha2022@gmail.com>
2023-11-23 17:32:42 +0300

    complete

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,13 +1,16 @@
 import React from "react";
 
-function ProjectItem({ name, about, technologies }) {
+function ProjectItem({ name, about, technologies, href }) {
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {technologies.map((technology) => (
+          <span key={technology}>{technology}</span>
+        ))}
       </div>
+      <a href={href}></a>
     </div>
   );
 }
